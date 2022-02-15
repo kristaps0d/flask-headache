bbe9d55d67bccf0061cf76d75a195dd8dcc10806
kristaps <kkrisapss.m@gmail.com>
2022-02-15 11:10:26 +0200

    undo: flask-dotenv removal

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 # Core packages
-import os
+import os, dotenv
 
 # Get root folder path
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Get enveiromental values
-secret_key = os.environ['SECRET_KEY']
+dotenv.load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
 
 if secret_key is None:
 	raise SystemExit('Missing "SECRET_KEY" from .env')
